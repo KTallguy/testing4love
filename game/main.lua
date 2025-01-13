efefdd466f3e4e6ab988c76f5cea96c2d8aebfe3
Kenan <ktallguy@gmail.com>
2025-01-13 13:17:57 +0900

    Initial tests, changed license, removed boilerplate from Readme, fixed launch paths

--- game/main.lua
+++ game/main.lua
@@ -1,9 +1,16 @@
 function love.load()
 end
 
+
+function sayNumber(num)
+	Output = "I like the number " .. num
+	return Output
+end
+
 function love.update()
-	print("Hello!")
+
 end
 
 function love.draw()
+	love.graphics.print(sayNumber(5234123), 100,100)
 end
