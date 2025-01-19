33639dfaf23db3d626da339a569aea788a1d596a
Kenan <ktallguy@gmail.com>
2025-01-19 11:51:41 +0900

    more lua related tests for learning Love2d

--- game/main.lua
+++ game/main.lua
@@ -13,6 +13,29 @@ function love.load()
 	enemy = Enemy()
 	listOfBullets = {}
 
+	circle = {}
+	circle.x = 100
+	circle.y = 100
+	circle.radius = 25
+	circle.speed = 200
+
+	arrow = {}
+	arrow.x = 200
+	arrow.y = 200
+	arrow.speed = 300
+	arrow.angle = 0
+	arrow.image = love.graphics.newImage("assets/arrow_right.png")
+	arrow.origin_x = arrow.image:getWidth() / 2
+	arrow.origin_y = arrow.image:getHeight() / 2
+
+
+	frames = {}
+	for i = 1,5 do
+		table.insert(frames, love.graphics.newImage("assets/jump" .. i .. ".png"))
+	end
+
+	currentFrame = 1
+
 	--love.graphics.setBackgroundColor(1,1,1)
 
 end
@@ -34,6 +57,31 @@ function love.update(dt)
 		end
 	end
 
+	currentFrame = currentFrame + 10 * dt
+	if currentFrame >= 6 then
+		currentFrame = 1
+	end
+
+	--love.mouse.getPosition() returns the x & y pos of mouse cursor
+	mouse_x, mouse_y = love.mouse.getPosition()
+	arrow.angle = math.atan2(mouse_y - arrow.y, mouse_x - arrow.x)
+	cos = math.cos(arrow.angle)
+	sin = math.sin(arrow.angle)
+
+	local distance = getDistance(arrow.x, arrow.y, mouse_x, mouse_y)
+	if distance < 400 then
+		arrow.x = arrow.x + arrow.speed * cos * (distance/100) * dt
+		arrow.y = arrow.y + arrow.speed * sin * (distance/100) * dt
+	end
+	-- angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
+	-- cos = math.cos(angle)
+	-- sin = math.sin(angle)
+
+	-- local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+	-- if distance < 400 then
+	-- 	circle.x = circle.x + circle.speed * cos * (distance/100) * dt
+	-- 	circle.y = circle.y + circle.speed * sin * (distance/100) * dt
+	-- end
 end
 
 
@@ -44,6 +92,41 @@ function love.draw()
 	for i,v in ipairs(listOfBullets) do
 		v:draw()
 	end
+
+	love.graphics.draw(frames[math.floor(currentFrame)], arrow.x, arrow.y)
+
+	-- --circle test
+	-- love.graphics.circle("line", circle.x, circle.y, circle.radius)
+	-- -- print angle
+	-- love.graphics.print("angle: " .. angle, 10, 10)
+
+	-- --lines to visualize the velocity
+	-- love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
+	-- love.graphics.line(circle.x, circle.y, circle.x, mouse_y)
+
+	-- --the angle
+	-- love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
+
+	-- --the distance -> hypotenuse (pythagorean theorum hehe)
+	-- local distance = getDistance(circle.x, circle.y, mouse_x, mouse_y)
+	-- love.graphics.circle("line", circle.x, circle.y, distance)
+
+	-- arrow draw test
+	love.graphics.draw(arrow.image, arrow.x, arrow.y, arrow.angle, 1, 1, arrow.origin_x, arrow.origin_y)
+	love.graphics.circle("fill", mouse_x, mouse_y, 5)
+	
+end
+
+function getDistance(x1, y1, x2, y2)
+	local horizontal_distance = x1 - x2
+	local vertical_distance = y1 - y2
+
+	local a = horizontal_distance ^2
+	local b = vertical_distance ^2
+
+	local c = a + b
+	local distance = math.sqrt(c)
+	return distance
 
 end
 
